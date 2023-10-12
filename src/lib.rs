cee9361174ac923221f896476b730803d6b66cb1
nikos efthias <nikos@mugsoft.io>
2023-10-12 03:25:06 +0300

    various improvements
    - no external deps use std lib OnceLock primitive
    - simplify structure
    - avoid extra vector allocations

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,11 @@
 //!
 //! `get_gender()` - use this to get the gender  
 
-use std::{
-    collections::HashMap,
-    env,
-    fs::File,
-    include,
-    io::{BufRead, BufReader},
-    path::Path,
-};
+mod load_names;
+
+use std::collections::HashMap;
+
+use load_names::get_names;
 
 type Names = HashMap<String, String>;
 
@@ -30,38 +27,34 @@ pub enum Gender {
 ///```
 /// use gender_guesser::Detector;
 ///
-/// let mut d = Detector::new();
+/// let d = Detector::new();
 /// d.get_gender("name");
 ///```
 pub struct Detector {
-    names: Names,
+    names: &'static Names,
 }
-include!(concat!(env!("OUT_DIR"), "/load_names.rs"));
 
+impl Default for Detector {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 impl Detector {
     pub fn new() -> Self {
-        let names = &NAMES;
-        let mut hmap: HashMap<String, String> = HashMap::new();
-        for (u1, u2) in names.iter() {
-            hmap.insert(u1.to_owned(), u2.to_owned());
-        }
-        Self { names: hmap }
+        Self { names: get_names() }
     }
 
-    pub fn get_gender(&mut self, name: &str) -> Gender {
-        if self.names.contains_key(name) {
-            let letter = self.names.get(name).unwrap();
-
-            match letter {
+    pub fn get_gender(&self, name: &str) -> Gender {
+        self.names
+            .get(name)
+            .map(|letter| match letter {
                 letter if letter == "M" => Gender::Male,
                 letter if letter == "F" => Gender::Female,
                 letter if letter == "?M" => Gender::MayBeMale,
                 letter if letter == "?F" => Gender::MayBeFemale,
                 _ => Gender::NotSure,
-            }
-        } else {
-            Gender::NotFound
-        }
+            })
+            .unwrap_or(Gender::NotFound)
     }
 }
 
@@ -70,47 +63,47 @@ mod tests_gender_guesser {
     use super::*;
     #[test]
     fn display_male() {
-        let mut d = Detector::new();
+        let d = Detector::new();
         assert_eq!(Gender::Male, d.get_gender("Radu"));
     }
 
     #[test]
     fn display_female() {
-        let mut d = Detector::new();
+        let d = Detector::new();
         assert_eq!(Gender::Female, d.get_gender("Ana"));
     }
 
     #[test]
     fn display_female_oposite() {
-        let mut d = Detector::new();
+        let d = Detector::new();
         assert_ne!(Gender::Female, d.get_gender("Radu"));
     }
 
     #[test]
     fn display_male_oposite() {
-        let mut d = Detector::new();
+        let d = Detector::new();
         assert_ne!(Gender::Male, d.get_gender("Ana"));
     }
 
     #[test]
     fn display_may_be_male() {
-        let mut d = Detector::new();
+        let d = Detector::new();
         assert_eq!(Gender::MayBeMale, d.get_gender("Adair"));
     }
     #[test]
     fn display_may_be_female() {
-        let mut d = Detector::new();
+        let d = Detector::new();
         assert_eq!(Gender::MayBeFemale, d.get_gender("Aaf"));
     }
 
     #[test]
     fn display_not_found() {
-        let mut d = Detector::new();
+        let d = Detector::new();
         assert_eq!(Gender::NotFound, d.get_gender("Adadadasdadasd"));
     }
     #[test]
     fn display_not_sure() {
-        let mut d = Detector::new();
+        let d = Detector::new();
         assert_eq!(Gender::NotSure, d.get_gender("Abbe"));
     }
 }
