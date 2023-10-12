2823ce3981221e72a71d78799c14d668352110cb
nikos efthias <nikos@mugsoft.io>
2023-10-12 03:25:06 +0300

    various improvements
    - no external deps use std lib OnceLock primitive
    - simplify structure
    - avoid extra vector allocations

--- src/load_names.rs
+++ src/load_names.rs
@@ -1,1 +1,25 @@
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader},
+    sync::OnceLock,
+};
 
+const FILE: &str = include_str!("nam_dict.txt");
+pub(crate) static NAMES: OnceLock<HashMap<String, String>> = OnceLock::new();
+pub(crate) fn get_names() -> &'static HashMap<String, String> {
+    NAMES.get_or_init(|| {
+        BufReader::new(FILE.as_bytes())
+            .lines()
+            .flatten()
+            .flat_map(|line| {
+                if line.starts_with('#') {
+                    return None;
+                }
+                let mut items = line.split_whitespace();
+                let item0 = items.next()?;
+                let item1 = items.next()?;
+                Some((item1.to_string(), item0.to_string()))
+            })
+            .collect()
+    })
+}
